a3d53545b90bb6981ae2a483cb39676d6016c24e
Phil Karn <karn@ka9q.net>
2026-05-17 19:42:41 -0400

    remove deadwood

--- debian/README.Debian.ex
+++
@@ -1,6 +0,0 @@
-ka9q-ft8 for Debian
-------------------
-
-<Possible notes regarding this package - if none, delete this file.>
-
- -- Phil Karn <karn@bart.ka9q.net>  Tue, 05 May 2026 02:46:00 -0700
--- debian/README.source.ex
+++
@@ -1,9 +0,0 @@
-ka9q-ft8 for Debian
-------------------
-
-<This file describes information about the source package, see Debian policy
-manual section 4.14. You WILL either need to modify or delete this file.>
-
-
-
- -- Phil Karn <karn@bart.ka9q.net>  Tue, 05 May 2026 02:46:00 -0700
